6a8d09125e13c59577b0593a560408985230872e
SkidBoi <kenzokay.business@gmail.com>
2025-08-17 20:38:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,15 +12,15 @@ try:
 
     if sys.platform.startswith("win"):
         os.system("cls")
-        print("Installing the python modules required for the RedTiger Tool:\n")
+        print("Installing the python modules required for the Jackpot Tool:\n")
         os.system("python -m pip install --upgrade pip")
         os.system("python -m pip install -r requirements.txt")
         OpenSites()
-        os.system("python RedTiger.py")
+        os.system("python Jackpot.py")
 
     elif sys.platform.startswith("linux"):
         os.system("clear")
-        print("Installing the python modules required for the RedTiger Tool:\n")
+        print("Installing the python modules required for the Jackpot Tool:\n")
         os.system("pip3 install --upgrade pip")
         os.system("pip3 install -r requirements.txt")
         OpenSites()
